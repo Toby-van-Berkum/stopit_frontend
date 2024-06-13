74943d59c206157373082313f9c376da20a067b5
Rolan <107186461+Kamikaze363@users.noreply.github.com>
2024-06-13 15:11:54 +0200

    added scrolling to login, register and checkin

--- lib/pages/checkin_page.dart
+++ lib/pages/checkin_page.dart
@@ -55,92 +55,94 @@ class _CheckInPageState extends State<CheckInPage> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              CustomSizedBox.large(),
-              Text("Daily Check Up", style: AppStyles.headerStyle()),
-              CustomSizedBox.large(),
-              Text("Did you smoke today?", style: AppStyles.labelStyle()),
-              CustomSizedBox.small(),
-              ToggleButtons(
-                direction: vertical ? Axis.vertical : Axis.horizontal,
-                onPressed: (int index) {
-                  setState(() {
-                    for (int i = 0; i < _hasSmoked.length; i++) {
-                      _hasSmoked[i] = i == index;
-                    }
-                    _hasSmokedValue = index == 0;
-                  });
-                },
-                borderRadius: const BorderRadius.all(Radius.circular(8)),
-                selectedBorderColor: AppColors.primaryColor,
-                fillColor: AppColors.primaryColor,
-                constraints: const BoxConstraints(
-                  minHeight: 40.0,
-                  minWidth: 80.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CustomSizedBox.large(),
+                Text("Daily Check Up", style: AppStyles.headerStyle()),
+                CustomSizedBox.large(),
+                Text("Did you smoke today?", style: AppStyles.labelStyle()),
+                CustomSizedBox.small(),
+                ToggleButtons(
+                  direction: vertical ? Axis.vertical : Axis.horizontal,
+                  onPressed: (int index) {
+                    setState(() {
+                      for (int i = 0; i < _hasSmoked.length; i++) {
+                        _hasSmoked[i] = i == index;
+                      }
+                      _hasSmokedValue = index == 0;
+                    });
+                  },
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  selectedBorderColor: AppColors.primaryColor,
+                  fillColor: AppColors.primaryColor,
+                  constraints: const BoxConstraints(
+                    minHeight: 40.0,
+                    minWidth: 80.0,
+                  ),
+                  isSelected: _hasSmoked,
+                  children: hasSmoked,
                 ),
-                isSelected: _hasSmoked,
-                children: hasSmoked,
-              ),
-              CustomSizedBox.medium(),
-              Text("How did your day go?", style: AppStyles.labelStyle()),
-              CustomSizedBox.small(),
-              TextFormField(
-                controller: _commentController,
-                keyboardType: TextInputType.multiline,
-                minLines: 1,
-                maxLines: 7,
-                maxLength: 512,
-                decoration: AppStyles.inputStyle("Comment"),
-              ),
-              const SizedBox(height: 10),
-              Text("How difficult was today?", style: AppStyles.labelStyle()),
-              CustomSizedBox.small(),
-              ToggleButtons(
-                direction: vertical ? Axis.vertical : Axis.horizontal,
-                onPressed: (int index) {
-                  setState(() {
-                    for (int i = 0; i < _difficulty.length; i++) {
-                      _difficulty[i] = i == index;
-                    }
-                    _difficultyValue = difficultyLevels[index];
-                  });
-                },
-                borderRadius: const BorderRadius.all(Radius.circular(8)),
-                selectedBorderColor: AppColors.primaryColor,
-                fillColor: AppColors.primaryColor,
-                constraints: BoxConstraints(
-                  minHeight: 40.0,
-                  minWidth: ScreenSizes.width(context) / 5.51,
+                CustomSizedBox.medium(),
+                Text("How did your day go?", style: AppStyles.labelStyle()),
+                CustomSizedBox.small(),
+                TextFormField(
+                  controller: _commentController,
+                  keyboardType: TextInputType.multiline,
+                  minLines: 1,
+                  maxLines: 7,
+                  maxLength: 512,
+                  decoration: AppStyles.inputStyle("Comment"),
                 ),
-                isSelected: _difficulty,
-                children: difficulty,
-              ),
-              CustomSizedBox.large(),
-              LargeButton(
-                buttonLabel: "Submit",
-                onPressed: () {
-                  // Save the values
-                  debugPrint('Comment: $_comment');
-                  debugPrint('Has smoked: $_hasSmokedValue');
-                  debugPrint('Difficulty: $_difficultyValue');
-                  //TODO: Fix false value pass on (passes null when not null)
+                const SizedBox(height: 10),
+                Text("How difficult was today?", style: AppStyles.labelStyle()),
+                CustomSizedBox.small(),
+                ToggleButtons(
+                  direction: vertical ? Axis.vertical : Axis.horizontal,
+                  onPressed: (int index) {
+                    setState(() {
+                      for (int i = 0; i < _difficulty.length; i++) {
+                        _difficulty[i] = i == index;
+                      }
+                      _difficultyValue = difficultyLevels[index];
+                    });
+                  },
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  selectedBorderColor: AppColors.primaryColor,
+                  fillColor: AppColors.primaryColor,
+                  constraints: BoxConstraints(
+                    minHeight: 40.0,
+                    minWidth: ScreenSizes.width(context) / 5.51,
+                  ),
+                  isSelected: _difficulty,
+                  children: difficulty,
+                ),
+                CustomSizedBox.large(),
+                LargeButton(
+                  buttonLabel: "Submit",
+                  onPressed: () {
+                    // Save the values
+                    debugPrint('Comment: $_comment');
+                    debugPrint('Has smoked: $_hasSmokedValue');
+                    debugPrint('Difficulty: $_difficultyValue');
+                    //TODO: Fix false value pass on (passes null when not null)
 
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const DashboardPage(title: AppTitle.title),
-                    ),
-                  );
-                },
-              ),
-            ],
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const DashboardPage(title: AppTitle.title),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
