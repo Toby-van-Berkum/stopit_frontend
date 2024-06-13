8da2edccb50618e51e79d257f340c2a4ef5e8f30
Toby-van-Berkum <s1131370@student.hsleiden.nl>
2024-06-13 16:39:18 +0200

    Updated App Label Updated RegExp - Register Page & Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'StopIt',
       theme: ThemeData(
         textTheme: AppFonts.poppins,
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
