f342102f30bd10c0624847d37757516c7278ddb0
Toby-van-Berkum <s1131370@student.hsleiden.nl>
2024-06-14 16:41:28 +0200

    Fixed Journal Page (mostly)

--- lib/pages/journal_page.dart
+++ lib/pages/journal_page.dart
@@ -1,34 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:stopit_frontend/globals.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:stopit_frontend/globals.dart';
 
 class JournalPage extends StatefulWidget {
+  const JournalPage({super.key, required this.title});
+  final String title;
+
   @override
   _JournalPageState createState() => _JournalPageState();
-
-  const JournalPage({super.key, required this.title});
-
-  final String title;
 }
 
 class _JournalPageState extends State<JournalPage> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  DateTime _firstDay = DateTime.now().subtract(Duration(days:30));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppTitle.title),
+        title: Text(widget.title),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
             TableCalendar(
-              // firstDay: DateTime.utc(2010, 10, 16),
-              firstDay: DateTime(DateTime.now().year - 3),
+              firstDay: _firstDay,
               lastDay: DateTime.now(),
               focusedDay: _focusedDay,
               calendarFormat: _calendarFormat,
@@ -70,8 +69,98 @@ class _JournalPageState extends State<JournalPage> {
                 formatButtonVisible: false,
                 titleCentered: true,
               ),
+              calendarBuilders: CalendarBuilders(
+                dowBuilder: (context, day) {
+                  if (day.weekday == DateTime.sunday) {
+                    return Center(
+                      child: Text(
+                        'Sun',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    );
+                  }
+                  return null;
+                },
+                selectedBuilder: (context, date, _) {
+                  return Container(
+                    margin: const EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      color: AppColors.white,
+                      border: Border.all(color: AppColors.primaryColor),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      '${date.day}',
+                      style: TextStyle(color: AppColors.black),
+                    ),
+                  );
+                },
+                todayBuilder: (context, date, _) {
+                  return Container(
+                    margin: const EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryColor,
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      '${date.day}',
+                      style: TextStyle(color: AppColors.black),
+                    ),
+                  );
+                },
+                markerBuilder: (context, date, events) {
+                  if (events.isNotEmpty) {
+                    return Positioned(
+                      right: 1,
+                      bottom: 1,
+                      child: _buildEventsMarker(date, events),
+                    );
+                  }
+                  return null;
+                },
+                defaultBuilder: (context, date, _) {
+                  // Check if the date is on or after _firstDay and before or on DateTime.now()
+                  if (date.isAfter(_firstDay.subtract(Duration(days: 1))) && date.isBefore(DateTime.now())) {
+                    return Container(
+                      margin: const EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(
+                        color: AppColors.primaryColor,
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        '${date.day}',
+                        style: TextStyle(color: AppColors.black),
+                      ),
+                    );
+                  }
+                  return null;
+                },
+              ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.blue,
+      ),
+      width: 16.0,
+      height: 16.0,
+      child: Center(
+        child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
         ),
       ),
     );
